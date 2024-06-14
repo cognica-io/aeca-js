c31a66622e56883509f7113c9bba3ac824945537
Sanghoun Choi <cozyhous@gmail.com>
2024-06-14 22:12:32 +0900

    Update version

--- src/system_metrics.ts
+++ src/system_metrics.ts
@@ -9,7 +9,7 @@ import * as proto from "@/proto/generated/system_metrics"
 import { Channel } from "./channel"
 import { GrpcClient } from "./client"
 
-type SystemMetricsSnapshot = proto.SystemMetricsSnapshot
+export type SystemMetricsSnapshot = proto.SystemMetricsSnapshot
 
 export class SystemMetrics extends GrpcClient<proto.SystemMetricsServiceClient> {
   constructor(channel: Channel, timeout: number | undefined = undefined) {
